b084e308b7baa91c86d606dd3d910136758cf606
JunboJ <junboz598@gmail.com>
2019-10-08 14:28:42 +1300

    learn about error types and debug nodejs

--- routes.js
+++ routes.js
@@ -5,14 +5,14 @@ function reqHandler(req, res) {
     const method = req.method;
     if (url === '/main') {
         res.write('<html>');
-        res.write('<head><title>Main page</title><head>');
+        res.write('<head><title>Main page</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
         res.write('</body>')
         res.write('</html>');
         return res.end();
     }
     
-    if (url === '/message' && method === 'POST') {
+    else if (url === '/message' && method === 'POST') {
         const body = [];
         req.on('data', (chunck) => {
             console.log(chunck);
@@ -25,16 +25,16 @@ function reqHandler(req, res) {
             fs.writeFile('message.txt', message, (err) => {
                 res.statusCode = 302;
                 res.setHeader('Location', '/');
-                return res.end();
+                res.end();
             });
         });
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
-    res.write('<head><title>Welcome to my first node web!</title><head>');
+    res.write('<head><title>Welcome to my first node web!</title></head>');
     res.write('<body><h1>Hello James, it has been a long time.</h1>');
     res.write('<h2>Now, please continue.</h2>');
-    res.write('<form action="/main" method="GET"><button type="submit">Main page</button></form>');
+    res.write('<a href="/main">Main page</a>');
     res.write('</html>');
     res.end();
 };

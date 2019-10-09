d03ba8779c7c83554d9569ac96e3a63d16a099b1
JunboJ <junboz598@gmail.com>
2019-10-09 19:16:05 +1300

    move on to express.js

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const http = require('http');
 
-const routes = require('./routes');
+const exp = require('express');
 
-const server = http.createServer(routes.handler);
+const app = exp();
+
+app.use((req, res, next) => {
+    console.log('the first middleware...');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('the second middleware...');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
